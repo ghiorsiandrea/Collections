ed616360e7140f15927bbd7baef0cb43843ab3c9
ghiorsiandrea <andreaghiorsims@gmail.com>
2022-07-29 18:09:07 -0300

    Se emprolijan las javadocs de forma de que sean lo mas acertivos posible, debido a que MyMap es una implementacion simplificada de Map que intenta abarcar los aspectos mas relevantes de la misma.

--- src/main/java/com/example/map/MyMap.java
+++ src/main/java/com/example/map/MyMap.java
@@ -1,6 +1,6 @@
 package com.example.map;
 
-public interface MyMap<K , V > {
+public interface MyMap<K, V> {
 
     // Query Operations
 
@@ -24,7 +24,7 @@ public interface MyMap<K , V > {
      * Returns {@code true} if this map contains a mapping for the
      * specified key.
      *
-     * @param   key   The key whose presence in this map is to be tested
+     * @param key The key whose presence in this map is to be tested
      * @return {@code true} if this map contains a mapping for the specified
      * key.
      */
@@ -37,21 +37,17 @@ public interface MyMap<K , V > {
      *
      * @param value value whose presence in this map is to be tested
      * @return {@code true} if this map maps one or more keys to the
-     *         specified value
+     * specified value
      */
     boolean containsValue(V value);
 
     /**
-     * {@inheritDoc}
-     *
-     * @implSpec
-     * This implementation iterates over {@code entrySet()} searching
+     * @throws ClassCastException   {@inheritDoc}
+     * @throws NullPointerException {@inheritDoc}
+     * @implSpec This implementation iterates over {@code entrySet()} searching
      * for an entry with the specified key.  If such an entry is found,
      * the entry's value is returned.  If the iteration terminates without
      * finding such an entry, {@code null} is returned.
-     *
-     * @throws ClassCastException            {@inheritDoc}
-     * @throws NullPointerException          {@inheritDoc}
      */
     V get(K key);
 
@@ -63,79 +59,53 @@ public interface MyMap<K , V > {
      * If the map previously contained a mapping for the key, the old
      * value is replaced.
      *
-     * @param key key with which the specified value is to be associated
+     * @param key   key with which the specified value is to be associated
      * @param value value to be associated with the specified key
      * @return the previous value associated with {@code key}, or
-     *         {@code null} if there was no mapping for {@code key}.
-     *         (A {@code null} return can also indicate that the map
-     *         previously associated {@code null} with {@code key}.)
+     * {@code null} if there was no mapping for {@code key}.
+     * (A {@code null} return can also indicate that the map
+     * previously associated {@code null} with {@code key}.)
      */
     V put(K key, V value);
 
     /**
-     * Removes the mapping for a key from this map if it is present
-     * (optional operation).   More formally, if this map contains a mapping
-     * from key {@code k} to value {@code v} such that
-     * {@code Objects.equals(key, k)}, that mapping
-     * is removed.  (The map can contain at most one such mapping.)
-     *
+     * Removes the mapping for a key from this map if it is present.
      * <p>Returns the value to which this map previously associated the key,
      * or {@code null} if the map contained no mapping for the key.
      *
-     * <p>If this map permits null values, then a return value of
-     * {@code null} does not <i>necessarily</i> indicate that the map
-     * contained no mapping for the key; it's also possible that the map
-     * explicitly mapped the key to {@code null}.
-     *
-     * <p>The map will not contain a mapping for the specified key once the
-     * call returns.
-     *
      * @param key key whose mapping is to be removed from the map
      * @return the previous value associated with {@code key}, or
-     *         {@code null} if there was no mapping for {@code key}.
-     * @throws UnsupportedOperationException if the {@code remove} operation
-     *         is not supported by this map
-     * @throws ClassCastException if the key is of an inappropriate type for
-     *         this map
+     * {@code null} if there was no mapping for {@code key}.
      * (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
-     * @throws NullPointerException if the specified key is null and this
-     *         map does not permit null keys
-     * (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
+     * @throws IllegalArgumentException if the specified key is null and this
+     *                                  map does not permit null keys
      */
     V remove(K key);
 
     // Bulk Operations
 
     /**
-     * Copies all of the mappings from the specified map to this map
+     * Copies all the mappings from the specified map to this map
      * (optional operation).  The effect of this call is equivalent to that
-     * of calling {@link #put(Object,Object) put(k, v)} on this map once
+     * of calling {@link #put(Object, Object) put(k, v)} on this map once
      * for each mapping from key {@code k} to value {@code v} in the
      * specified map.  The behavior of this operation is undefined if the
      * specified map is modified while the operation is in progress.
      *
      * @param m mappings to be stored in this map
-     * @throws UnsupportedOperationException if the {@code putAll} operation
-     *         is not supported by this map
-     * @throws ClassCastException if the class of a key or value in the
-     *         specified map prevents it from being stored in this map
-     * @throws NullPointerException if the specified map is null, or if
-     *         this map does not permit null keys or values, and the
-     *         specified map contains null keys or values
+     * @throws NullPointerException     if the specified map is null, or if
+     *                                  this map does not permit null keys or values, and the
+     *                                  specified map contains null keys or values
      * @throws IllegalArgumentException if some property of a key or value in
-     *         the specified map prevents it from being stored in this map
+     *                                  the specified map prevents it from being stored in this map
      */
     void putAll(MyMap<? extends K, ? extends V> m);
 
     /**
      * Removes all the mappings from this map (optional operation).
      * The map will be empty after this call returns.
-     *
-     * @throws UnsupportedOperationException if the {@code clear} operation
-     *         is not supported by this map
      */
     void clear();
-
 
 
 }
--- src/main/java/com/example/map/MyMapImpl1.java
+++ src/main/java/com/example/map/MyMapImpl1.java
@@ -19,64 +19,26 @@ public class MyMapImpl1<K, V> implements MyMap<K, V> {
 
     // Query Operations
 
-    /**
-     * Returns the number of key-value mappings in this map.  If the
-     * map contains more than {@code Integer.MAX_VALUE} elements, returns
-     * {@code Integer.MAX_VALUE}.
-     *
-     * @return the number of key-value mappings in this map
-     */
     @Override
     public int size() {
         return keys.size();
     }
 
-    /**
-     * Returns {@code true} if this map contains no key-value mappings.
-     *
-     * @return {@code true} if this map contains no key-value mappings
-     */
     @Override
     public boolean isEmpty() {
         return size() == 0;
     }
 
-    /**
-     * Returns {@code true} if this map contains a mapping for the
-     * specified key.
-     *
-     * @param key The key whose presence in this map is to be tested
-     * @return {@code true} if this map contains a mapping for the specified
-     * key.
-     */
     @Override
     public boolean containsKey(K key) {
         return keys.contains(key);
     }
 
-    /**
-     * Returns {@code true} if this map maps one or more keys to the
-     * specified value.
-     *
-     * @param value value whose presence in this map is to be tested
-     * @return {@code true} if this map maps one or more keys to the
-     * specified value
-     */
     @Override
     public boolean containsValue(V value) {
         return values.contains(value);
     }
 
-    /**
-     * {@inheritDoc}
-     *
-     * @throws ClassCastException   {@inheritDoc}
-     * @throws NullPointerException {@inheritDoc}
-     * @implSpec This implementation iterates over {@code entrySet()} searching
-     * for an entry with the specified key.  If such an entry is found,
-     * the entry's value is returned.  If the iteration terminates without
-     * finding such an entry, {@code null} is returned.
-     */
     @Override
     public V get(K key) {
 
@@ -97,18 +59,6 @@ public class MyMapImpl1<K, V> implements MyMap<K, V> {
 
     // Modification Operations
 
-    /**
-     * Associates the specified value with the specified key in this map.
-     * If the map previously contained a mapping for the key, the old
-     * value is replaced.
-     *
-     * @param key   key with which the specified value is to be associated
-     * @param value value to be associated with the specified key
-     * @return the previous value associated with {@code key}, or
-     * {@code null} if there was no mapping for {@code key}.
-     * (A {@code null} return can also indicate that the map
-     * previously associated {@code null} with {@code key}.)
-     */
     @Override
     public V put(K key, V value) {
 
@@ -118,31 +68,6 @@ public class MyMapImpl1<K, V> implements MyMap<K, V> {
         return oldValue;
     }
 
-    /**
-     * Removes the mapping for a key from this map if it is present
-     * (optional operation).   More formally, if this map contains a mapping
-     * from key {@code k} to value {@code v} such that
-     * {@code Objects.equals(key, k)}, that mapping
-     * is removed.  (The map can contain at most one such mapping.)
-     *
-     * <p>Returns the value to which this map previously associated the key,
-     * or {@code null} if the map contained no mapping for the key.
-     *
-     * <p>If this map permits null values, then a return value of
-     * {@code null} does not <i>necessarily</i> indicate that the map
-     * contained no mapping for the key; it's also possible that the map
-     * explicitly mapped the key to {@code null}.
-     *
-     * <p>The map will not contain a mapping for the specified key once the
-     * call returns.
-     *
-     * @param key key whose mapping is to be removed from the map
-     * @return the previous value associated with {@code key}, or
-     * {@code null} if there was no mapping for {@code key}.
-     * (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
-     * @throws IllegalArgumentException if the specified key is null and this
-     *                                  map does not permit null keys
-     */
     @Override
     public V remove(K key) {
         if (key == null) {
@@ -176,10 +101,6 @@ public class MyMapImpl1<K, V> implements MyMap<K, V> {
      * specified map is modified while the operation is in progress.
      *
      * @param m mappings to be stored in this map
-     * @throws UnsupportedOperationException if the {@code putAll} operation
-     *                                       is not supported by this map
-     * @throws ClassCastException            if the class of a key or value in the
-     *                                       specified map prevents it from being stored in this map
      * @throws NullPointerException          if the specified map is null, or if
      *                                       this map does not permit null keys or values, and the
      *                                       specified map contains null keys or values
@@ -194,9 +115,6 @@ public class MyMapImpl1<K, V> implements MyMap<K, V> {
     /**
      * Removes all the mappings from this map (optional operation).
      * The map will be empty after this call returns.
-     *
-     * @throws UnsupportedOperationException if the {@code clear} operation
-     *                                       is not supported by this map
      */
     @Override
     public void clear() {
@@ -205,19 +123,15 @@ public class MyMapImpl1<K, V> implements MyMap<K, V> {
 
     @Override
     public String toString() {
-        return "MyMapImpl1{" + "\n" +
-                "elements=" + "\n" + keys + "\n" + values + ",\n" +
-                '}';
-    }
-
-
-//    public String toStringa() {
 //        return "MyMapImpl1{" + "\n" +
 //                "elements=" + "\n" + keys + "\n" + values + ",\n" +
 //                '}';
-//        for (int i = 0; i < size(); i++) {
-//            i = i + 1;
-//        }
-//    }
+
+        for (int i = 0; i < size(); i++) {
+            String map = keys + values.toString();
+            return map;
+        }
+        return null;
+    }
 
 }
